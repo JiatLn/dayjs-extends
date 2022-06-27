9e5decaa6d8dfa5de88a2ce6c6a9002035562c46
cx33 <lne128gene980@163.com>
2022-06-27 23:34:53 +0800

    chore: remove lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import dayjs from 'dayjs';
-declare module 'dayjs' {
-    function init(): string;
-    function between(start: string | Date, end: string | Date): dayjs.Dayjs;
-}
-declare const _default: (day: typeof dayjs) => void;
-export default _default;
