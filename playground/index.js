0aa80175aa880abb2ad2d4ffa86c8bada59d77ac
林佳涛 <lne128gene980@163.com>
2022-06-28 10:59:27 +0800

    chore: rm playground

--- playground/index.js
+++
@@ -1,8 +0,0 @@
-import dayjsExt from '../lib/index.js'
-
-const de = dayjsExt()
-
-const date1 = de.random.between('2018-01-01', '2018-01-31').format('YYYY-MM-DD')
-const date2 = de.getToday()
-const date3 = de.sayHi()
-console.table([date1, date2, date3])
